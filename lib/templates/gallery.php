c0f11259213a37b23821bb8f71ee30fa0287b50d
Diego Islas Ocampo <dev@hyuchia.com>
2018-08-29 22:15:06 -0500

    Update Ikaros framework

--- lib/templates/gallery.php
+++ lib/templates/gallery.php
@@ -55,6 +55,21 @@
 				],
 
 				[
+					'name' => 'Dreams of Desire',
+					'description' => 'This is a story about power and corruption with a mystery to solve!
+					You play as the so called Main Character!
+
+					Due to unfortunate circumstances, it looks like he is forced to go to military school.
+					Our hero however, has other ideas. He never wanted to be a soldier. As things start to look inevitable, he stumbles upon an old book about the ways of the mind.
+					With the help of newfound knowledge he can change the course of his life. But how will he do it, and what\'s the price he has to pay? ',
+					'url' => 'https://www.gamesofdesire.com/3d/dreams-of-desire-episode-1/',
+					'website' => 'https://www.patreon.com/lewdlab/',
+					'cover' => 'dreams-of-desire.jpg',
+					'creator' => 'Lewdlab\'s Dr. Sin',
+					'tags' => 'NSFW'
+				],
+
+				[
 					'name' => 'Gavel',
 					'description' => '',
 					'url' => 'https://ded.increpare.com/~locus/gavel/',
@@ -63,6 +78,27 @@
 					'creator' => 'Stephen Lavelle',
 					'tags' => ''
 				],
+
+				[
+					'name' => 'Melody',
+					'description' => 'You will be spending time with Melody every day. Aside from one-on-one lessons you will have with her, you will also be accompanying her at various music related activities. If all goes well, you may even be able to go out on a date with her. And if that goes well too, it could lead to something even better!',
+					'url' => 'https://www.gamesofdesire.com/quest/melody-porn-game/',
+					'website' => 'https://www.patreon.com/mrdotsgames/',
+					'cover' => 'melody.jpg',
+					'creator' => 'Mr. Dots',
+					'tags' => 'NSFW'
+				],
+
+				[
+					'name' => 'Milfy City',
+					'description' => 'A university student moves to a new city to study. He rents a house with a couple of other students and local business owners. Following on from this he asks his university Professor out on a date, but is rejected. The game begins here as a dating simulator, with options to pursue romantic relationships with another student, a couple of local business owners, and a couple of University staff.',
+					'url' => 'https://www.gamesofdesire.com/3d/milfy-city-v0-4/',
+					'website' => 'https://www.patreon.com/icstor/',
+					'cover' => 'milfy-city.png',
+					'creator' => 'ICSTOR',
+					'tags' => 'NSFW'
+				],
+
 				[
 					'name' => 'Urban Voyeur',
 					'description' => 'You play as a young successful male doctor who has just moved in to New York to work in a private clinic.
@@ -72,7 +108,7 @@
 					'website' => 'https://www.patreon.com/cesargames',
 					'cover' => 'urban-voyeur.png',
 					'creator' => 'Cesar Games',
-					'tags' => 'NSFW, Voyeurism'
+					'tags' => 'NSFW'
 				]
 			];
 
